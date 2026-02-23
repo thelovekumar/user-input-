df5cab318f8b40d98384041237725bae71ea0988
Love Kumar <lovesoni2024@gmail.com>
2026-02-23 10:36:21 +0530

    take user input
    This is my fiest user input program

---
+++ user_input.py
@@ -0,0 +1,13 @@
+
+print("Program start")
+
+
+print()
+first_num = int(input("Enter your first number:"))
+second_num = int(input("Enter your second number:"))
+
+print()
+print(f"{first_num} + {second_num} = {first_num + second_num}")
+
+print()
+print("Program end")
